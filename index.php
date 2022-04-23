a520d0f837827904dd5f8cb11e70fa14174e170c
DmytroPshen <mailforajob49@gmail.com>
2022-04-23 12:20:21 +0300

    start commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    $name = 'Victor';
+    $age = 20;
+    $hobby = 'Swimming';
+    $isMarried = true;
+    $pet = 'cat';
+    $petName = 'Barsik';
+
+    echo $name . "<br>";
+    echo $age . "<br>";
+    echo $hobby . "<br>";
+    echo $isMarried . "<br>";
+    echo $pet . "<br>";
+    echo $petName . "<br><br>";
+
+    $name1 = 'Oleg';
+    $age1 = 34;
+    $hobby1 = 'running';
+    $isMarried1 = false;
+    $pet1 = 'dog';
+    $petName1 = 'Rex';
+
+    echo $name1 . "<br>";
+    echo $age1 . "<br>";
+    echo $hobby1 . "<br>";
+    echo $isMarried1 . "<br>";
+    echo $pet1 . "<br>";
+    echo $petName1 . "<br><br>";
